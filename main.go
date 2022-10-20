990c5d70d822aa186a6365c04517f381751f19b6
rehanahmedch <chrehan@gmail.com>
2022-10-20 21:08:29 +0500

    Initiating the tutorial

--- main.go
+++ main.go
@@ -1,10 +1,72 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type User struct {
+	name    string
+	address string
+}
 
 func main() {
 
-	fmt.Println("Rehan Ahmed")
+	// 	const (
+	// 		i = iota
+	// 		a = iota
+	// 	)
+
+	// var name *string = new(string)
+	// *name = "Rehan Ahmed"
+	// fmt.Println(*name)
+	// name := "Rehan Ahmed"
+
+	// ptr := &name
+
+	// fmt.Println(ptr)
+
+	// name := "Rehan Ahmed"
+	// ptr = &name
+	// fmt.Println(name)
+
+	// i := 10
+	// g := 10.26
+	// s := float64(i) + g
+	// fmt.Println(s)
+
+	// fmt.Println(i, a)
+
+	// ==================>  Slice: Work just like list in Python but with a single variable type
+	// arr := []int{1, 2, 3}
+
+	// arr = append(arr, 5, 6, 7)
+
+	// fmt.Println(arr[:])
+
+	// ==================>  Maps: Can work with any data type
+
+	// var mp = make(map[string]int)
+	// var mp = map[string]string{}
+	// mp["One"] = "one"
+	// fmt.Println(mp)
+
+	// val, exsistance := mp["One"]
+	// fmt.Print("Value: ", val, "\nExsistance: ", exsistance)
+	// delete(mp, "One")
+	// val, exsistance := mp["One"]
+	// fmt.Print("Value: ", val, "\nExsistance: ", exsistance)
+
+	// ==================>  Struct
+
+	// var u User
+	// u.name = "Rehan"
+	// u.address = "Address"
+	// fmt.Println(u) // return json consists all the values
+	// fmt.Println(u.name) // return the value only
+
+	// u := User{
+	// 	name:    "Rehan",
+	// 	address: "Address",
+	// }
+	// fmt.Println(u)
+
+	
 }
